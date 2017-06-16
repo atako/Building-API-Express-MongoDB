d61a62af94336aa0e1be1b98e8ebe6ac15df816d
Dmitry Ryumkin <riumkin@gmail.com>
2017-06-16 15:56:32 +0300

    Separate test databases

--- app.js
+++ app.js
@@ -5,7 +5,10 @@ const routes = require('./routes/routes');
 const app = express();
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://mongo/muber');
+if (process.env.NODE_ENV !== 'test') {
+  mongoose.connect('mongodb://mongo/muber');
+}
+
 
 app.use(bodyParser.json());
 routes(app);
